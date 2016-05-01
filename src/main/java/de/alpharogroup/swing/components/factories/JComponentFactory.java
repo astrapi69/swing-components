d69c43faad4da88dd97d3bfb878b294814b71b36
Asterios Raptis <asterios.raptis@gmx.net>
2016-05-01 17:06:55 +0200

    updated parent version

--- src/main/java/de/alpharogroup/swing/components/factories/JComponentFactory.java
+++ src/main/java/de/alpharogroup/swing/components/factories/JComponentFactory.java
@@ -3,6 +3,7 @@ package de.alpharogroup.swing.components.factories;
 import java.awt.Component;
 import java.awt.event.MouseListener;
 
+import javax.swing.JInternalFrame;
 import javax.swing.JMenuItem;
 import javax.swing.JPopupMenu;
 
@@ -31,14 +32,15 @@ public final class JComponentFactory {
 	 *            popup menu.
 	 * @return the new {@link JPopupMenu}.
 	 */
-	public static JPopupMenu newJPopupMenu(String label) {
+	public static JPopupMenu newJPopupMenu(final String label) {
 		final JPopupMenu popup = new JPopupMenu(label);
 		return popup;
 	}
 
 	/**
 	 * Factory method for create a <code>JPopupMenu</code> that will be add a
-	 * <code>MouseListener</code> to the given <code>Component</code> and an array of <code>JMenuItem</code> that will be added to the popup.
+	 * <code>MouseListener</code> to the given <code>Component</code> and an
+	 * array of <code>JMenuItem</code> that will be added to the popup.
 	 *
 	 * @param component
 	 *            the component
@@ -46,29 +48,53 @@ public final class JComponentFactory {
 	 *            the <code>JMenuItem</code>s
 	 * @return the new {@link JPopupMenu}.
 	 */
-	public static JPopupMenu newJPopupMenu(Component component, JMenuItem... items) {
+	public static JPopupMenu newJPopupMenu(final Component component, final JMenuItem... items) {
 		return newJPopupMenu("", component, items);
 	}
 
 	/**
 	 * Factory method for create a <code>JPopupMenu</code> that will be add a
-	 * <code>MouseListener</code> to the given <code>Component</code> and an array of <code>JMenuItem</code> that will be added to the popup.
+	 * <code>MouseListener</code> to the given <code>Component</code> and an
+	 * array of <code>JMenuItem</code> that will be added to the popup.
 	 *
+	 * @param label
+	 *            the label
 	 * @param component
 	 *            the component
 	 * @param items
 	 *            the <code>JMenuItem</code>s
 	 * @return the new {@link JPopupMenu}.
 	 */
-	public static JPopupMenu newJPopupMenu(String label, Component component, JMenuItem... items) {
+	public static JPopupMenu newJPopupMenu(final String label, final Component component, final JMenuItem... items) {
 		// Create the popup menu.
-		JPopupMenu popup = newJPopupMenu(label);
-		for (JMenuItem jMenuItem : items) {
+		final JPopupMenu popup = newJPopupMenu(label);
+		for (final JMenuItem jMenuItem : items) {
 			popup.add(jMenuItem);
 		}
 		// Add listener to the component so the popup menu can come up.
-		MouseListener popupListener = new PopupListener(popup);
+		final MouseListener popupListener = new PopupListener(popup);
 		component.addMouseListener(popupListener);
 		return popup;
+	}
+
+	/**
+	 * Factory method for create a {@link JInternalFrame} object.
+	 *
+	 * @param title
+	 *            the title
+	 * @param resizable
+	 *            the resizable
+	 * @param closable
+	 *            the closable
+	 * @param maximizable
+	 *            the maximizable
+	 * @param iconifiable
+	 *            the iconifiable
+	 * @return the new {@link JInternalFrame}.
+	 */
+	public static JInternalFrame newInternalFrame(final String title, final boolean resizable, final boolean closable,
+			final boolean maximizable, final boolean iconifiable) {
+		final JInternalFrame internalFrame = new JInternalFrame(title, resizable, closable, maximizable, iconifiable);
+		return internalFrame;
 	}
 }
--- src/main/java/de/alpharogroup/swing/menu/popup/listeners/PopupListener.java
+++ src/main/java/de/alpharogroup/swing/menu/popup/listeners/PopupListener.java
@@ -6,57 +6,54 @@ import java.awt.event.MouseEvent;
 import javax.swing.JPopupMenu;
 
 /**
- * The listener interface for receiving popup events.
- * The class that is interested in processing a popup
- * event implements this interface, and the object created
- * with that class is registered with a component using the
- * component's <code>addPopupListener<code> method. When
- * the popup event occurs, that object's appropriate
- * method is invoked.
- *
- * @see PopupEvent
+ * The listener interface for receiving popup events. The class that is
+ * interested in processing a popup event implements this interface, and the
+ * object created with that class is registered with a component using the
+ * component's addPopupListener method. When the popup event occurs, that
+ * object's appropriate method is invoked.
  */
 public class PopupListener extends MouseAdapter {
 
 	/** The popup menu. */
 	private final JPopupMenu popupMenu;
-	
+
 	/**
 	 * Instantiates a new popup listener.
 	 *
-	 * @param menu the menu
+	 * @param menu
+	 *            the menu
 	 */
-	public PopupListener(JPopupMenu menu) {
+	public PopupListener(final JPopupMenu menu) {
 		super();
-		this.popupMenu = menu;
+		popupMenu = menu;
 	}
 
 	/**
 	 * {@inheritDoc}
 	 */
 	@Override
-    public void mousePressed(MouseEvent e) {
-        onShowPopup(e);
-    }
+	public void mousePressed(final MouseEvent e) {
+		onShowPopup(e);
+	}
 
 	/**
 	 * {@inheritDoc}
 	 */
 	@Override
-    public void mouseReleased(MouseEvent e) {
-        onShowPopup(e);
-    }
+	public void mouseReleased(final MouseEvent e) {
+		onShowPopup(e);
+	}
 
-    /**
-     * Callback method that is called on show popup.
-     *
-     * @param e the mouse event
-     */
-    protected void onShowPopup(MouseEvent e) {
-        if (e.isPopupTrigger()) {
-        	System.out.println(e.getSource());
-        	popupMenu.show(e.getComponent(),
-                       e.getX(), e.getY());
-        }
-    }
+	/**
+	 * Callback method that is called on show popup.
+	 *
+	 * @param e
+	 *            the mouse event
+	 */
+	protected void onShowPopup(final MouseEvent e) {
+		if (e.isPopupTrigger()) {
+			System.out.println(e.getSource());
+			popupMenu.show(e.getComponent(), e.getX(), e.getY());
+		}
+	}
 }
